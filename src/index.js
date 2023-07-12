4928467b0d5a01dc9877e1d95c16f6400eff3d7f
leandro1990 <gomez.leandro1990@gmail.com>
2023-07-12 02:07:11 -0300

    [feature] - implement conditional rendering for selected category and handle navigate state in app, create categories and products sreen

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
-import { FlatList, SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-import { CategoryItem, Header } from './components';
-import CATEGORIES from './constants/data/categories.json';
+import { Header } from './components';
+import { Categories, Products } from './screens';
+import { useState } from 'react';
 
 export default function App() {
-    const onSelectCategory = (categoryId) => {
-        console.warn({ categoryId });
+    const [isCategorySelected, setIsCategorySelected] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+
+    const headerTitle = isCategorySelected ? 'Products' : 'Categories';
+
+    const onHandleSelectCategory = (categoryId) => {
+        setSelectedCategory(categoryId);
+        setIsCategorySelected(!isCategorySelected);
+    }
+    const onHandleNavigate = () => {
+        setIsCategorySelected(!isCategorySelected);
+        setSelectedCategory(null);
     };
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.container}>
-                <Header title="Categories" />
-                <FlatList
-                    data={CATEGORIES}
-                    style={styles.categoryContainer}
-                    contentContainerStyle={styles.listCategory}
-                    renderItem={({ item }) => <CategoryItem {...item} onSelectCategory={onSelectCategory} />}
-                    keyExtractor={(item) => item.id}
-                    showsVerticalScrollIndicator={false}
-                />
+                <Header title={headerTitle} />
+                {isCategorySelected ? (
+                    <Products onHandleGoBack={onHandleNavigate} />
+                ) : (
+                    <Categories onSelectCategory={onHandleSelectCategory} />
+                )}
             </View>
         </SafeAreaView>
     );
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-    },
-    categoryContainer: {
-        marginTop: 15,
-        marginHorizontal: 15,
-    },
-    listCategory: {
-        gap: 15,
-        paddingBottom: 15,
     },
 });
