a1698d8be47b610d1d48d352ccc84a9628f6dd69
leandro1990 <gomez.leandro1990@gmail.com>
2023-07-13 23:12:19 -0300

    [feacture] - add logic to show filtered products by category and search

--- src/screens/products/index.jsx
+++ src/screens/products/index.jsx
@@ -7,41 +7,66 @@ import { COLORS } from "../../themes";
 import { useState } from "react";
 
 function Product({ onHandleGoBack, categoryId }) {
-
     const [search, setSearch] = useState('');
+    const [filteredProducts, setFilteredProducts] = useState([]);
     const [borderColor, setBorderColor] = useState(COLORS.primary);
-    const onHanleBlur = () => {};
+    const onHandleBlur = () => { };
     const onHandleChangeText = (text) => {
         setSearch(text);
+        filterBySearch(text);
     };
-    const onHandleFocus = () => {};
+    const onHandleFocus = () => { };
 
-    const filteredProducts = PRODUCTS.filter((product) => product.categoryId === categoryId );
+    const filteredProductsByCategory = PRODUCTS.filter(
+        (product) => product.categoryId === categoryId
+    );
+
+    const filterBySearch = (query) => {
+        let updatedProductList = [...filteredProductsByCategory];
+
+        updatedProductList = updatedProductList.filter((product) => {
+            return product.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+        });
+
+        setFilteredProducts(updatedProductList);
+    };
+
+    const clearSearch = () => {
+        setSearch('');
+        setFilteredProducts([]);
+    };
+
     return (
-    <View style={styles.container}>
-        <TouchableOpacity style={styles.goBack}  onPress={onHandleGoBack}>
-            <Ionicons name='arrow-back-circle' size={30} color={COLORS.black}/>
-            <Text style={styles.goBackText}>Go back</Text>
-        </TouchableOpacity>
-    <View style={styles.header}>
-            <Input
-                onHandleBlur={onHanleBlur}
-                onHandleChangeText={onHandleChangeText}
-                onHandleFocus={onHandleFocus}
-                value={search}
-                placeholder="Search"
-                borderColor={borderColor}
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.goBack} onPress={onHandleGoBack}>
+                <Ionicons onPress={clearSearch} name="arrow-back-circle" size={30} color={COLORS.black} />
+                <Text style={styles.goBackText}>Go back</Text>
+            </TouchableOpacity>
+            <View style={styles.header}>
+                <Input
+                    onHandleBlur={onHandleBlur}
+                    onHandleChangeText={onHandleChangeText}
+                    onHandleFocus={onHandleFocus}
+                    value={search}
+                    placeholder="Search"
+                    borderColor={borderColor}
+                />
+                {search.length > 0 && (
+                    <Ionicons onPress={clearSearch} name="close-circle" size={30} color={COLORS.black} />
+                )}
+            </View>
+            <FlatList
+                style={styles.products}
+                data={search.length > 0 ? filteredProducts : filteredProductsByCategory}
+                renderItem={({ item }) => <Text>{item.name}</Text>}
+                keyExtractor={(item) => item.id.toString()}
             />
-            <Ionicons name="search-circle" size={40} color={COLORS.text} />
-            {search.length > 0 && <Ionicons name="close-circle" size={40} color={COLORS.text} />}
-    </View>
-    <FlatList
-        data={filteredProducts}
-        renderItem={({ item}) => <Text>{item.name}</Text>}
-        keyExtractor={(item) => item.id.toString()}
-    />
-    </View>
+            {filteredProducts.length === 0 && search.length > 0 && (
+                <View style={styles.notFound}>
+                    <Text style={styles.notFoundText}>No products found</Text>
+                </View>
+            )}
+        </View>
     );
 }
-
 export default Product;
