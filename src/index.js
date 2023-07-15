8ab8ae2ccbe1f4f95938bb48286816f3373142e1
leandro1990 <gomez.leandro1990@gmail.com>
2023-07-15 15:58:12 -0300

    [feacture] - create stack navigator and implement root navigation

--- src/index.js
+++ src/index.js
@@ -1,65 +1,60 @@
-import { useFonts } from 'expo-font';
-import { useState } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, Button, ActivityIndicator } from 'react-native';
+import { useFonts } from "expo-font";
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, View, Text, Button, ActivityIndicator } from "react-native";
 
-import { Header } from './components';
-import { Categories, Products } from './screens';
-import { FONTS, COLORS } from './themes';
+import { Header } from "./components";
+import RootNavigator from "./navigations";
+import { Categories, Products } from "./screens";
+import { FONTS, COLORS } from "./themes";
 
-const categoryDefault = {
-    categoryId: null,
-    color: COLORS.primary,
-};
+// const categoryDefault = {
+//   categoryId: null,
+//   color: COLORS.primary,
+// };
+
 export default function App() {
-    const [loaded] = useFonts({
-        [FONTS.regular]: require('../assets/fonts/ClearSans-Regular.ttf'),
-        [FONTS.bold]: require('../assets/fonts/ClearSans-Bold.ttf'),
-        [FONTS.medium]: require('../assets/fonts/ClearSans-Medium.ttf'),
-        [FONTS.light]: require('../assets/fonts/ClearSans-Light.ttf'),
-    });
-    const [isCategorySelected, setIsCategorySelected] = useState(false);
-    const [selectedCategory, setSelectedCategory] = useState(categoryDefault);
+  const [loaded] = useFonts({
+    [FONTS.regular]: require("../assets/fonts/ClearSans-Regular.ttf"),
+    [FONTS.bold]: require("../assets/fonts/ClearSans-Bold.ttf"),
+    [FONTS.medium]: require("../assets/fonts/ClearSans-Medium.ttf"),
+    [FONTS.light]: require("../assets/fonts/ClearSans-Light.ttf"),
+  });
+  // const [isCategorySelected, setIsCategorySelected] = useState(false);
+  // const [selectedCategory, setSelectedCategory] = useState(categoryDefault);
 
-    const headerTitle = isCategorySelected ? 'Products' : 'Categories';
+  // const headerTitle = isCategorySelected ? 'Products' : 'Categories';
 
-    const onHandleSelectCategory = ({ categoryId, color }) => {
-        setSelectedCategory({ categoryId, color });
-        setIsCategorySelected(!isCategorySelected);
-    };
-    const onHandleNavigate = () => {
-        setIsCategorySelected(!isCategorySelected);
-        setSelectedCategory(categoryDefault);
-    };
+  // const onHandleSelectCategory = ({ categoryId, color }) => {
+  //   setSelectedCategory({ categoryId, color });
+  //   setIsCategorySelected(!isCategorySelected);
+  // };
+  // const onHandleNavigate = () => {
+  //   setIsCategorySelected(!isCategorySelected);
+  //   setSelectedCategory(categoryDefault);
+  // };
 
-    if (!loaded) {
-        return (
-            <View style={styles.loaderContainer}>
-                <ActivityIndicator color={COLORS.primary} size="large" />
-            </View>
-        );
-    }
-
+  if (!loaded) {
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.container}>
-                <Header title={headerTitle} style={{ backgroundColor: selectedCategory.color }} />
-                {isCategorySelected ? (
-                    <Products onHandleGoBack={onHandleNavigate} categorySelected={selectedCategory} />
-                ) : (
-                    <Categories onSelectCategory={onHandleSelectCategory} />
-                )}
-            </View>
-        </SafeAreaView>
+      <View style={styles.loaderContainer}>
+        <ActivityIndicator color={COLORS.primary} size="large" />
+      </View>
     );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <RootNavigator />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    loaderContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+  },
+  loaderContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
